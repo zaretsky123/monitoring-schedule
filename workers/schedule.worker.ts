e83cf609fb2cceb29e98c8bc56279da3e7b693ba
zaretsky123 <Arturzaretskyy333@gmail.com>
2026-09-20 10:53:39 -0500

    Run schedule generator in worker

--- workers/schedule.worker.ts
+++ workers/schedule.worker.ts
@@ -1,14 +1,17 @@
 /// <reference lib="webworker" />
 
 import { solveSchedule } from "../lib/schedule/solver";
+import { generateSchedule } from "../lib/schedule/generator";
 
-type WorkerRequest = Parameters<typeof solveSchedule>[0];
+type WorkerRequest =
+  | ({ action?: "rearrange" } & Parameters<typeof solveSchedule>[0])
+  | ({ action: "generate" } & Parameters<typeof generateSchedule>[0]);
 
 const workerScope = self as DedicatedWorkerGlobalScope;
 
 workerScope.onmessage = (event: MessageEvent<WorkerRequest>) => {
   try {
-    const result = solveSchedule(event.data);
+    const result = event.data.action === "generate" ? generateSchedule(event.data) : solveSchedule(event.data);
     workerScope.postMessage({ type: "result", result });
   } catch (error) {
     workerScope.postMessage({
